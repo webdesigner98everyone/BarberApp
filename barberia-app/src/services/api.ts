f08090c0fd8e69179864758c7550c087184d82f9
webdesigner98everyone <luisfoguz1198@gmail.com>
2026-04-02 02:30:09 -0500

    Implementacion de Variables de entorno

--- barberia-app/src/services/api.ts
+++ barberia-app/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.7:3000/api',
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use(async (config) => {
